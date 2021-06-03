5c18747636e4d92e1036c498f3dcd97bcb4f356a
Phil Ganney <philipganney@gmail.com>
2021-06-03 08:21:30 +0100

    Started actually building game + renamed mainStyles
    During this I realised I needed two separate major CSS files - one for the actual game, and one for the general purpose stuff that is in the landing page as well. (For "Separation of Concerns"). So I created gameStyles.css and renamed mainStyles.css to generalStyles.css (in the hope that the purpose is more clear)
    
    Game isn't playable at this point yet, but I have put in a game area, an element that can stand in for a character while I figure out movement, and buttons for movement that all call a function move(dir) - passing in a letter to represent the direction of movement

--- SRATestAreaV0-0-3a.js
+++ SRATestAreaV0-0-3a.js
@@ -1,6 +1,22 @@
 //This file should only have stuff used in test area pages. 
 //File should be renamed at least on each main version increment, plus any time we have issues with cacheing.
 
+function move(dir){ //dir = direction
+	//alert("Should move " + dir);
+	//TODO: switch case would probably be better
+	if (dir == "u"){
+		alert("u");
+		//TODO: find #mockPC coords - add a value to move it up
+	} else if (dir == "l"){
+		alert("l");
+	} else if (dir == "d"){
+		alert("d");
+	} else if (dir == "r"){
+		alert("r");
+	};
+	
+}
+
 function toggleWelcome(){
 	if (document.getElementById("welcome").classList.contains("showing")){
 		hideViaClass("welcome");
