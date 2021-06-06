722d6dc09ba3e74abb01439cfebe6d760c6465bc
Phil Ganney <philipganney@gmail.com>
2021-06-06 14:23:22 +0100

    Added sims reference joke to the code box

--- SRAHomePage.js
+++ SRAHomePage.js
@@ -25,6 +25,8 @@ function codeUnlockFunc(){
 		alert("Nice password! - but it's not correct - try demanding entry in three words but combined into one for 7 characters, all lower case");
 	} else if (codeAttempt.includes("RHCP")){
 		alert("by the way I tried to say I'd be there, waiting for");
+	} else if (codeAttempt.includes("osebud")){ //Sims "rosebud" cheat - I've left off the r as an easy way to allow for people putting a capital R instead - though equally "nosebud" would pass this lol.
+		alert("1000 Simoleans added to your characters wallet. Simoleans aren't the currency in SRA so they are useless!");
 	} else if (codeAttempt == "CAJJLLLBCK" || codeAttempt == "FLCIKLMODO" || codeAttempt == "GINLNNIIJL"){ //lemmings level codes fun 1 and fun 30 and mayhem 30 respectively
 		alert("Level codes aren't implemented yet, but try niemtel backwards instead");
 	} else if (codeAttempt.includes("letmein") || codeAttempt == "Iwannaplay"|| codeAttempt.includes("let me in")){//display the hidden tab when "letmein" or "Iwannaplay" , or anything with the words "let me in" such as "please let me in" or "let me in please" is used as a code, and the user confirms our agreement
