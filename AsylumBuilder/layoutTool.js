5d1e79ee59ec2bbb09457d8477d9c1fffa350212
Phil Ganney <philipganney@gmail.com>
2021-10-10 17:50:17 +0100

    Asylum tool WIP:  starts of blank Asylum generation

--- AsylumBuilder/layoutTool.js
+++ AsylumBuilder/layoutTool.js
@@ -1,6 +1,52 @@
+var asylumData = {
+	"rooms":{
+	
+	},
+	"defaultRoom": {
+		"bounds": { //Bounds are the edges of the playable area for a room, in px, and what kind of boundary is there - just wall, doorway, archway etc. To create these I took the values used for the first room used in the original asylum (room 10 - Rec3), and then converted them to all full walls - much of this will be overriden during editing anyway, but this gives us our basic structure.
+			"top": {
+				"val": "99",   //val is short for value - in this case the amount of px from the top of the movement area
+				"known": "no", //no means not known to the player character
+				"type":"wall", //wall = continuous wall with no archway or door
+			},
+			"right": {
+				"val": "658", //val is short for value - in this case the amount of px from the left edge of the movement area
+				"known": "no", //no means not known to the player character
+				"type":"wall", //wall = continuous wall with no archway or door
+			},
+			"bottom": {
+				"val": "439",  //val is short for value - in this case the amount of px from the top of the movement area
+				"known": "no", //no means not known to the player character
+				"type":"wall", //wall = continuous wall with no archway or door
+			},
+			"left": {
+				"val": "90",   //val is short for value - in this case the amount of px from the left edge of the movement area
+				"known": "no", //no means not known to the player character
+				"type":"wall", //wall = continuous wall with no archway or door
+			},
+		},
+		"bumpCoords": {
+			
+		}
+	},
+			
+	
+}
+
+function createRooms(){
+	
+	for (let i = 0; i < 100; i++) {
+		//alert(parseInt(i));
+		
+		asylumData.rooms[parseInt(i)] = {};
+	}
+	showViaClass("asylumGrid");
+}
+
 function viewCell(cellID){
 	//alert("we're still building this tool");
 	hideAllXClassShowY("rightPanelView", "cellView");
+	
 }
 
 /** Our good old trusty show and hide functions that we use everywhere!!
